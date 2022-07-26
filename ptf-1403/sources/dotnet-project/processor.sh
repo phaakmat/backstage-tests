3817b78df4869afccd387040275d9b4d4552bac9
Pascal Haakmat <pascalhaakmat@funda.nl>
2022-07-26 16:13:37 +0200

    Test

--- ptf-1403/sources/dotnet-project/processor.sh
+++ ptf-1403/sources/dotnet-project/processor.sh
@@ -1,8 +1,8 @@
 env
 
 mkdir -p $OUTPUT_PATH
-cp $TARGET_PATH/* $OUTPUT_PATH
-cp $SOURCE_PATH/* $OUTPUT_PATH
+cp -r $TARGET_PATH/* $OUTPUT_PATH
+cp -r $SOURCE_PATH/* $OUTPUT_PATH
 
 cd $OUTPUT_PATH && dotnet sln add $PROJECT_NAME/*.csproj
 
